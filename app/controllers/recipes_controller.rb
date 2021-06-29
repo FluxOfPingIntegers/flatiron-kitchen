539ef4a8ecd0e229eb646e2b86e84be9d25953d8
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-06-29 17:24:16 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,31 +1,42 @@
 class RecipesController < ApplicationController
 
     def index
-
-    end
-
-    def new
-
-    end
-
-    def create
-
-    end
-
-    def show
-
-    end
-
-    def edit
-
-    end
-
-    def update
-
-    end
-
-    def destroy
-
-    end
-
+        @recipes = Recipe.all
+      end
+  
+      def new
+        @recipe = Recipe.new
+        @ingredients = Ingredient.all
+      end
+  
+      def create
+        recipe = Recipe.create(recipe_params)
+        binding.pry
+        redirect_to recipe
+      end
+  
+      def show
+        @recipe = Recipe.find(params[:id])
+      end
+  
+      def edit
+        @recipe = Recipe.find(params[:id])
+      end
+  
+      def update
+        recipe = Recipe.find(params[:id])
+        Recipe.update(recipe_params)
+        redirect_to recipe
+      end
+  
+      def destroy
+  
+      end
+  
+      
+      def recipe_params
+        params[:recipe][:ingredient_ids] = params[:recipe][:ingredient_ids].select {|i| !i.empty?}
+        params.require(:recipe).permit(:name, :ingredient_ids)
+      end
+  
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,9 @@
+require 'pry'
 class Recipe < ApplicationRecord
     validates :name, presence: true
     validates :name, uniqueness: true
   
     has_many :recipe_ingredients
     has_many :ingredients, through: :recipe_ingredients
+    accepts_nested_attributes_for :ingredients, :recipe_ingredients
 end
